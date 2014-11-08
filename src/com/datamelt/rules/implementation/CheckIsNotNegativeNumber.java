63480701df3bdaf410e807bd24070ef00e9f0cb3
Uwe Geercken <uwe.geercken@web.de>
2014-11-08 19:55:15 +0100

    Update CheckIsNotNegativeNumber.java
    update incorrect documentation

--- src/com/datamelt/rules/implementation/CheckIsNotNegativeNumber.java
+++ src/com/datamelt/rules/implementation/CheckIsNotNegativeNumber.java
@@ -1,12 +1,12 @@
 /*
- * Created on 15.09.2006
+ * Created on 15.09.2014
  *
  * all code by uwe geercken
  */
 package com.datamelt.rules.implementation;
 
 /**
- * checks if a given value is smaller than zero (0)
+ * checks if a given value is not smaller than zero (0)
  * 
  * <p>
  * The first argument of a method is always the value of the field that one wants to check. The second argument is either another field 
@@ -21,7 +21,8 @@ package com.datamelt.rules.implementation;
 public class CheckIsNotNegativeNumber extends GenericCheck
 {
 	/**
-     * checks, if the integer is a number smaller than zero
+     * checks, if the integer is a number not smaller than zero
+     * meaning that it is greater or equal zero
      */
     public static boolean evaluate(int value1)
     {
@@ -36,7 +37,8 @@ public class CheckIsNotNegativeNumber extends GenericCheck
     }
     
 	/**
-     * checks, if the long is a number smaller than zero
+     * checks, if the long is a number not smaller than zero
+     * meaning that it is greater or equal zero
      */
     public static boolean evaluate(long value1)
     {
@@ -51,7 +53,8 @@ public class CheckIsNotNegativeNumber extends GenericCheck
     }
     
 	/**
-     * checks, if the double is a number smaller than zero
+     * checks, if the double is a number not smaller than zero
+     * meaning that it is greater or equal zero
      */
     public static boolean evaluate(double value1)
     {
@@ -66,7 +69,8 @@ public class CheckIsNotNegativeNumber extends GenericCheck
     }
     
 	/**
-     * checks, if the float is a number smaller than zero
+     * checks, if the float is a number not smaller than zero
+     * meaning that it is greater or equal zero
      */
     public static boolean evaluate(float value1)
     {
