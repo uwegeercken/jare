72ab20a79e61e7270300f61d4d49813fd62c229b
Uwe Geercken <uwe.geercken@web.de>
2019-05-14 22:02:59 +0200

    sample test case

---
+++ src/main/java/com/datamelt/rules/implementation/CheckContainsTest.java
@@ -0,0 +1,14 @@
+package com.datamelt.rules.implementation;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CheckContainsTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
